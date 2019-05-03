7c7d80c59de05c924d200474d6bb96387b3ace82
唐鸣泽丨 <oncwnuCk7Itnt5AuadKVHns9PxRk@git.weixin.qq.com>
2019-05-03 15:20:43 +0800

    收藏图书

---
+++ pages/book-collected/book-collected.js
@@ -0,0 +1,100 @@
+// pages/book-collected/book-collected.js
+const conllections = require('../../utils/collection_books')
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        collections: [],
+        wxId:'oR5YB5SwlSZS0m-RqCXFMhkxAVr0',
+        pageSize:5,
+        pageNum:0
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.loadCollectionList();
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+    /*取消收藏*/
+    delCollection: function (bookId) {
+        console.log(bookId)
+        var userId=wx.getStorageSync('third_Session');
+        console.log(userId);
+
+    },
+    /*加载收藏列表*/
+    loadCollectionList:function(){
+        console.log('加载收藏列表——————————————')
+        console.log('——————————————')
+        var _this=this;
+        var aimdarr=[];
+        var promias= conllections.getConllections(this.data.wxId,this.data.pageSize,this.data.pageNum);
+        promias.then((res)=>{
+            console.log(res);
+            aimdarr=res.data.data.resultList;
+            var conarr=this.data.collections;
+            conarr=conarr.concat(conarr,aimdarr)
+            console.log(conarr);
+            console.log(res.data.data.resultList);
+            this.setData({
+                collections:conarr,
+                pageNum:_this.data.pageNum+1
+            })
+            console.log(_this.data.collections[1]);
+        }).catch((e)=>{
+
+        })
+    }
+})
