7d59cbf00ba7702f450a60e4d3ec60d8b94d0b16
唐鸣泽丨 <oncwnuck7itnt5auadkvhns9pxrk@git.weixin.qq.com>
2019-05-03 22:50:18 +0800

    收藏图书 取消收藏功能完善

--- pages/book-collected/book-collected.js
+++ pages/book-collected/book-collected.js
@@ -7,9 +7,9 @@ Page({
      */
     data: {
         collections: [],
-        wxId:'oR5YB5SwlSZS0m-RqCXFMhkxAVr0',
-        pageSize:5,
-        pageNum:0
+        wxId: 'oR5YB5SwlSZS0m-RqCXFMhkxAVr0',
+        pageSize: 5,
+        pageNum: 0
     },
 
     /**
@@ -69,44 +69,54 @@ Page({
     },
     /*取消收藏*/
     delCollection: function (totol) {
-        var bookCode=totol.target.dataset.hi;
-        var userId=wx.getStorageSync('third_Session')||this.data.wxId;
-        console.log(userId,bookCode);
-        conllections.deleteCollection(userId,bookCode)
-            .then((res)=>{
+        var bookCode = totol.target.dataset.hi;
+        var userId = wx.getStorageSync('third_Session') || this.data.wxId;
+        console.log(userId, bookCode,bookCode.split(';')[0]);
+        var _this = this
+        conllections.deleteCollection(userId, bookCode.split(';')[0])
+            .then((res) => {
                 console.log(res);
                 wx.showToast({
                     title: res.data.msg,
                     icon: 'success',
                     duration: 2000
-                })
-                totol.display=false;
-                this.loadCollectionList();
-            }).catch((e)=>{
+                });
+                totol.display = false;
+
+                var collearr = _this.data.collections;
+                var aimFlag= collearr.filter((e)=>{
+                    if(bookCode.split(';')[1]!=e.bookCode)
+                        return true;
+                });
+                _this.setData({
+                    collections:aimFlag
+                });
+                console.log(_this.data.collections);
+            }).catch((e) => {
 
         })
 
     },
     /*加载收藏列表*/
-    loadCollectionList:function(){
+    loadCollectionList: function () {
         console.log('加载收藏列表——————————————')
         console.log('——————————————')
-        var _this=this;
-        var aimdarr=[];
-        var promias= conllections.getConllections(this.data.wxId,this.data.pageSize,this.data.pageNum);
-        promias.then((res)=>{
+        var _this = this;
+        var aimdarr = [];
+        var promias = conllections.getConllections(this.data.wxId, this.data.pageSize, this.data.pageNum);
+        promias.then((res) => {
             console.log(res);
-            aimdarr=res.data.data.resultList;
-            var conarr=this.data.collections;
-            conarr=conarr.concat(conarr,aimdarr)
+            aimdarr = res.data.data.resultList;
+            var conarr = this.data.collections;
+            conarr = conarr.concat(conarr, aimdarr)
             console.log(conarr);
             console.log(res.data.data.resultList);
             this.setData({
-                collections:conarr,
-                pageNum:_this.data.pageNum+1
+                collections: conarr,
+                pageNum: _this.data.pageNum + 1
             })
             console.log(_this.data.collections[1]);
-        }).catch((e)=>{
+        }).catch((e) => {
 
         })
     }
